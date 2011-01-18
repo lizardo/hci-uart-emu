ddd9e2c80f02e9f3209e119f2c6e5855cb53454c
Anderson Lizardo <anderson.lizardo@openbossa.org>
2011-01-18 00:31:01 -0400

    Initial version
    This initial version allows to run "hciconfig hciX up" and
    "hciconfig hciX down" without errors.

---
+++ hci_uart_emu.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python
+# Author: Anderson Lizardo <anderson.lizardo AT openbossa.org>
+# License: GPL v3
+
+import sys
+import socket
+import select
+import struct
+
+def dump_data(data, incoming=True):
+    # NOTE: flags is inverted because flow is PTS -> dongle
+    flags = 0x00 if incoming else 0x01
+    if ord(data[0]) in [0x01, 0x04]:
+        flags |= 0x02
+
+    # size, len, flags, drops, ts
+    btsnoop_pkt = struct.pack(">LLLLQ", len(data), len(data), flags, 0, 0)
+    dump.write(btsnoop_pkt + data)
+
+HCI_COMMAND_PKT = 1
+HCI_EVENT_PKT = 4
+
+def handle_data(data):
+    pkt_type, = struct.unpack("B", data[0])
+    idx = 1
+
+    if pkt_type == HCI_COMMAND_PKT:
+        # HCI Command Packet
+        hci_command_hdr = "<HB"
+        opcode, plen = struct.unpack_from(hci_command_hdr, data, idx)
+        idx += struct.calcsize(hci_command_hdr)
+        pdata, = struct.unpack("%ds" % plen, data[idx:])
+        return (opcode, pdata)
+    else:
+        raise NotImplementedError, "pkt_type: %d" % pkt_type
+
+def parse_opcode(opcode):
+    return (opcode >> 10, opcode & 0x03ff)
+
+def cmd_complete(opcode, rparams):
+    eparams = struct.pack("<BH", 1, opcode) + rparams
+    return struct.pack("<BBB", HCI_EVENT_PKT, 0x0E, len(eparams)) + eparams
+
+def build_event(opcode, pdata):
+    ogf, ocf = parse_opcode(opcode)
+
+    if ogf == 0x04:
+        # Informational Parameters
+        if ocf == 0x0001:
+            # Read Local Version Information
+            rparams = struct.pack("<BBHBHH", 0x00, 6, 0x0000, 6, 65535, 0x0000)
+            return cmd_complete(opcode, rparams)
+        elif ocf == 0x0003:
+            # Read Local Supported Features
+            rparams = struct.pack("B8s", 0x00, "\xff\xff\xff\xfe\xfb\xff\x7b\x87")
+            return cmd_complete(opcode, rparams)
+        elif ocf == 0x0005:
+            # Read Buffer Size
+            rparams = struct.pack("<BHBHH", 0x00, 2048, 255, 1, 1)
+            return cmd_complete(opcode, rparams)
+        elif ocf == 0x0009:
+            # Read BD_ADDR
+            rparams = struct.pack("<B6s", 0x00, "\xfe\xca\xfe\xca\xfe\xca")
+            return cmd_complete(opcode, rparams)
+    elif ogf == 0x03:
+        # Controller & Baseband Commands
+        if ocf == 0x0003:
+            # Reset
+            return cmd_complete(opcode, "\x00")
+        elif ocf == 0x0005:
+            # Set Event Filter
+            # FIXME: implement event filtering
+            return cmd_complete(opcode, "\x00")
+        elif ocf == 0x0014:
+            # Read Local Name
+            name = "dummy"
+            name += "\x00" * (248 - len(name))
+            rparams = struct.pack("<B248s", 0x00, name)
+            return cmd_complete(opcode, rparams)
+        elif ocf == 0x0016:
+            # Write Connection Accept Timeout
+            return cmd_complete(opcode, "\x00")
+        elif ocf == 0x0018:
+            # Write Page Timeout
+            return cmd_complete(opcode, "\x00")
+        elif ocf == 0x0023:
+            # Read Class of Device
+            rparams = struct.pack("<B3s", 0x00, "\x00\x01\x04")
+            return cmd_complete(opcode, rparams)
+        elif ocf == 0x0025:
+            # Read Voice Setting
+            rparams = struct.pack("<BH", 0x00, 0x0000)
+            return cmd_complete(opcode, rparams)
+    elif ogf == 0x08:
+        # LE Controller Commands
+        if ocf == 0x0002:
+            # LE Read Buffer Size
+            rparams = struct.pack("<BHB", 0x00, 2048, 1)
+            return cmd_complete(opcode, rparams)
+
+    raise NotImplementedError, "ogf = 0x%02x, ocf = 0x%04x" % (ogf, ocf)
+
+def wait_data():
+    buf = ""
+    while True:
+        nfds = p.poll()
+        if not nfds:
+            continue
+
+        data = sock.recv(4096, socket.MSG_DONTWAIT)
+        assert data
+        print "XXX: data:", data.encode("hex")
+
+        buf += data
+        try:
+            ret = handle_data(buf)
+        except struct.error:
+            continue
+        else:
+            dump_data(buf)
+            buf = ""
+
+        print "XXX: handle_data:", ret
+        data = build_event(*ret)
+        sock.send(data)
+        dump_data(data, False)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print >>sys.stderr, "Usage: %s <btsnoop.dump>" % sys.argv[0]
+        sys.exit(1)
+
+    btsnoop_hdr = struct.pack(">8BLL", 0x62, 0x74, 0x73, 0x6e, 0x6f, 0x6f, 0x70, 0x00, 1, 1002)
+    dump = open(sys.argv[1], "wb")
+    dump.write(btsnoop_hdr)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("10.100.1.1", 9999))
+
+    p = select.poll()
+    p.register(sock, select.POLLIN)
+
+    try:
+        wait_data()
+    except KeyboardInterrupt:
+        pass
+
+    print >>sys.stderr, "\nExiting..."
+
+    sock.close()
+    dump.close()
