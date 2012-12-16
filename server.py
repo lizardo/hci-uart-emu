e701c55f46921630cf31b1380da9bf4434690474
Anderson Lizardo <anderson.lizardo@openbossa.org>
2012-12-16 11:01:32 -0400

    server: Simplify HCI command complete event building

--- server.py
+++ server.py
@@ -21,97 +21,50 @@ class DummyBT(asynchat.async_chat):
         assert d.packet_indicator == "COMMAND"
         ocf = d.packet.opcode.ocf
 
+        def cmd_complete(rparams):
+            return Container(
+                packet_indicator = 'EVENT',
+                packet = Container(
+                    evt = 'CMD_COMPLETE',
+                    params = Container(
+                        opcode = d.packet.opcode,
+                        ncmd = 1,
+                        rparams = rparams,
+                    )
+                )
+            )
+
         if ocf == "READ_LOCAL_FEATURES":
-            c = Container(
-                packet_indicator = 'EVENT',
-                packet = Container(
-                    evt = 'CMD_COMPLETE',
-                    params = Container(
-                        opcode = d.packet.opcode,
-                        ncmd = 1,
-                        rparams = Container(
-                            status = 0,
-                            features = [164, 8, 0, 192, 88, 30, 123, 131]),
-                        )
-                    )
-                )
+            c = cmd_complete(Container(status = 0,
+                features = [164, 8, 0, 192, 88, 30, 123, 131],
+            ))
         elif ocf == "READ_LOCAL_VERSION":
-            c = Container(
-                packet_indicator = 'EVENT',
-                packet = Container(
-                    evt = 'CMD_COMPLETE',
-                    params = Container(
-                        opcode = d.packet.opcode,
-                        ncmd = 1,
-                        rparams = Container(
-                            status = 0,
-                            hci_ver = 6,
-                            hci_rev = 0,
-                            lmp_subver = 0,
-                            lmp_ver = 6,
-                            manufacturer = 63),
-                        )
-                    )
-                )
+            c = cmd_complete(Container(status = 0,
+                hci_ver = 6,
+                hci_rev = 0,
+                lmp_subver = 0,
+                lmp_ver = 6,
+                manufacturer = 63,
+            ))
         elif ocf == "READ_BD_ADDR":
-            c = Container(
-                packet_indicator = 'EVENT',
-                packet = Container(
-                    evt = 'CMD_COMPLETE',
-                    params = Container(
-                        opcode = d.packet.opcode,
-                        ncmd = 1,
-                        rparams = Container(
-                            status = 0,
-                            bdaddr = [0x42, 0x00, 0x00, 0x01, 0xAA, 0x00]),
-                        )
-                    )
-                )
+            c = cmd_complete(Container(status = 0,
+                bdaddr = [0x42, 0x00, 0x00, 0x01, 0xAA, 0x00],
+            ))
         elif ocf == "READ_BUFFER_SIZE":
-            c = Container(
-                packet_indicator = 'EVENT',
-                packet = Container(
-                    evt = 'CMD_COMPLETE',
-                    params = Container(
-                        opcode = d.packet.opcode,
-                        ncmd = 1,
-                        rparams = Container(
-                            status = 0,
-                            acl_mtu = 192,
-                            acl_max_pkt = 1,
-                            sco_max_pkt = 0,
-                            sco_mtu = 0),
-                        )
-                    )
-                )
+            c = cmd_complete(Container(status = 0,
+                acl_mtu = 192,
+                acl_max_pkt = 1,
+                sco_max_pkt = 0,
+                sco_mtu = 0,
+            ))
         elif ocf == "READ_CLASS_OF_DEV":
-            c = Container(
-                packet_indicator = 'EVENT',
-                packet = Container(
-                    evt = 'CMD_COMPLETE',
-                    params = Container(
-                        opcode = d.packet.opcode,
-                        ncmd = 1,
-                        rparams = Container(
-                            status = 0,
-                            dev_class = [0, 0, 0]),
-                        )
-                    )
-                )
+            c = cmd_complete(Container(status = 0,
+                dev_class = [0, 0, 0],
+            ))
         elif ocf == "READ_LOCAL_NAME":
-            c = Container(
-                packet_indicator = 'EVENT',
-                packet = Container(
-                    evt = 'CMD_COMPLETE',
-                    params = Container(
-                        opcode = d.packet.opcode,
-                        ncmd = 1,
-                        rparams = Container(
-                            status = 0,
-                            name = "test"),
-                        )
-                    )
-                )
+            c = cmd_complete(Container(status = 0,
+                name = "test",
+            ))
         else:
             raise NotImplementedError, "Unsupported packet: %s" % d
 
